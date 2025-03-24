0b6d745bafa0822ed4eef0538b943df4af8b476d
faseehaliasif <faseehali507@gmail.com>
2025-03-24 08:14:04 +0000

    set environemnt

---
+++ interview_platform/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "AiPrep",
+  description: "An Ai-Powered platfrom for preparing mock interviews.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monaSans.className
+          
+        } antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
